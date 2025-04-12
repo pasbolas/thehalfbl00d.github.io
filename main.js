5199eaecb62eb7e97d624c37f2e8517b0e0bf709
saujix <d24126231@mytudublin.ie>
2025-04-12 20:35:55 +0100

    some minor changes

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 let audio = new Audio('assets/audio/ballin.mp3');
 let audio2= new Audio('assets/audio/mouseclick.mp3');
+let yoink= new Audio('assets/audio/yoink.mp3');
+let count = 0;
 
 document.querySelector('#ballin').addEventListener('click',function()
 {
     audio2.play();
     audio.play();
-    while(audio.paused && audio2.paused);
+    while(audio.paused && audio2.paused);   
+})
+document.querySelector('#skrilllogo').addEventListener('click',function()
+{
+    yoink.play(); 
+})
 
-    
+document.querySelector('#me').addEventListener('click',function(){
+    count++;
+    let audio = new Audio('assets/audio/i\ do\ not\ consent.mp3');
+    if (count % 5 == 0){
+        audio.play();
+    }
 })
